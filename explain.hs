b1814f7c7d38303d6557feb6884e786bba6e2333
Zjoris <jorisgalema@hotmail.com>
2024-05-20 12:34:06 +0200

    extra parameter for explainPrp for the offset that distuingishes calls from secrets (needs another look since it does not work correctly yet). Added function that prints list of (explained) propositions for a knowledge struct, only for the vocab at the moment.

--- explain.hs
+++ explain.hs
@@ -1,12 +1,30 @@
 module Explain where
 
 import SMCDEL.Examples.GossipS5
+import SMCDEL.Symbolic.S5
 import SMCDEL.Language
 import Test.QuickCheck
 
-explainCallPrp :: Int -> Prp -> String
-explainCallPrp n (P call) = "Agent " ++ (show i) ++ " called with agent " ++ (show j)
-   where 
-    i = (call - 100) `quot` n
-    j = (call - 100) `rem` n
+-- in  : number of gossipers (Int), offset that distinguishes "secret" props from "call" props (Int), and the actual prop to decipher (Prp).
+-- out : String containing meaning of proposition (String)
+explainPrp :: Int -> Int -> Prp -> String
+explainPrp n offset (P prop) | prop > offset = "Agent " ++ (show i) ++ " called with agent " ++ (show j)
+                                 | otherwise     = "S_{"++(show i') ++ "}"++ (show j')
+                                    where 
+                                    i  = (prop - offset) `quot` 10
+                                    j  = (prop - offset) `rem` 10
+                                    i' = prop `quot` n
+                                    j' = prop `rem` n
+
+
+-- explainKS :: KnowStruct -> Int -> Int -> String 
+-- explainKS (KnS voc sLaw obs) n offset = vocString 
+--    where vocString =
+
+kSProps :: KnowStruct -> Int -> Int -> IO ()
+kSProps (KnS voc sLaw obs) n offset = do 
+   putStrLn "vocab: "
+   mapM_ (putStrLn . ((++) " --  ") . (explainPrp n offset)) voc
+   putStrLn "State Law: "
+   putStrLn "Observables: "
 
