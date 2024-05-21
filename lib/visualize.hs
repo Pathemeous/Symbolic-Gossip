41cfd4501d48ffc878be6c58fa6919fb2e769f00
Madeleine Gignoux <madeleine.gignoux@gmail.com>
2024-05-21 17:22:08 +0200

    make Visualize

---
+++ lib/visualize.hs
@@ -0,0 +1,10 @@
+module Visualize where
+
+import SMCDEL.Examples.GossipS5
+import SMCDEL.Symbolic.S5
+import SMCDEL.Language
+import SMCDEL.Other.BDD2Form
+import Test.QuickCheck
+import Data.Maybe
+import SMCDEL.Internal.TexDisplay
+
